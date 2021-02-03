9647008c747bd9e452f0baf16b6f6a0bc7c95209
urszulakulon <urszulakulon@gmail.com>
2021-02-03 18:51:05 +0100

    test case add

--- cypress/integration/saleor_dashboard/loginTestDashNeg.js
+++ cypress/integration/saleor_dashboard/loginTestDashNeg.js
@@ -5,13 +5,42 @@ context('As an admin user I would d like to log in to my dashboard account', () 
         cy.visit('https://demo.saleor.io/dashboard/')
     })
 
-    it('should login to Saleor Dashboard with correct credentials SAL-DASH01002', () => {
+    it('should not login to Saleor Dashboard with clear inputs SAL-DASH01002', () => {
 
+        cy.get('[data-test="email"]').clear();
+        cy.get('[data-test="password"]').clear();
         cy.get('[data-test=submit]')
             .click()
-
-        cy.get('[data-test=userMenu]')
-            .should('be.visible')
-
     })
+
+    it('should not login to Saleor Dashboard with wrong e-mail SAL-DASH01002', () => {
+
+        cy.get('[data-test=email]').clear().type('testowy@test.test')
+        cy.get('[data-test=submit]')
+            .click()
+    })
+
+    it('should not login to Saleor Dashboard with wrong e-mail type string SAL-DASH01002', () => {
+
+        cy.get('[data-test=email]').clear().type('asdasdasd')
+        cy.get('[data-test=submit]')
+            .click()
+    })
+
+    it('should not login to Saleor Dashboard with proper e-mail and no password SAL-DASH01002', () => {
+
+        cy.get('[data-test=email]')
+        cy.get('[data-test="password"]').clear();
+        cy.get('[data-test=submit]')
+            .click()
+    })
+
+    it('should login to Saleor Dashboard with proper e-mail and proper password password SAL-DASH01002', () => {
+
+        cy.get('[data-test=email]')
+        cy.get('[data-test="password"]')
+        cy.get('[data-test=submit]')
+            .click()
+    })
+
 })
